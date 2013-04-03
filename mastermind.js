79c4082ab0e3b7bb66ba17d6421e0d590f79a4bd
Motofumi Takamura <takamura@timedia.co.jp>
2013-04-03 19:06:17 +0900

    Add a utility to log messages

--- mastermind.js
+++ mastermind.js
@@ -1,3 +1,8 @@
 (function () {
+  function log(message) {
+    var $line = $('<div class="line"></div>');
+    $line.text(message);
+    $('#message').prepend($line);
+  }
 })();
 // vim: expandtab softtabstop=2 shiftwidth=2
